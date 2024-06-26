470c85fac432c45dda433e4813504193d1cb25d6
Dannelysbeth <danielaf@onet.pl>
2024-06-26 23:33:10 +0200

    changed main part 2

--- src/main/java/dannelysbeth/auth/OAuthJava.java
+++ src/main/java/dannelysbeth/auth/OAuthJava.java
@@ -2,6 +2,6 @@ package dannelysbeth.auth;
 
 public class OAuthJava {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello oAuth!");
     }
 }
