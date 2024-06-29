de288359cc48e3198410d35234ffa4059519ad9e
Dannelysbeth <danielaf@onet.pl>
2024-06-29 23:53:56 +0200

    updated user

--- src/main/java/dannelysbeth/auth/model/User.java
+++ src/main/java/dannelysbeth/auth/model/User.java
@@ -1,4 +1,46 @@
 package dannelysbeth.auth.model;
 
-public class User {
+
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Setter
+public class User implements UserDetails {
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
 }
