8d58e4fc97a1f25aa22a9525a470e41a2efc402f
Dannelysbeth <danielaf@onet.pl>
2024-07-07 22:32:50 +0200

    added service implementation update

--- src/main/java/dannelysbeth/auth/service/implementation/UserServiceImpl.java
+++ src/main/java/dannelysbeth/auth/service/implementation/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package dannelysbeth.auth.service.implementation;
 
+import dannelysbeth.auth.repository.UserRepository;
 import dannelysbeth.auth.service.declaration.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
 }
