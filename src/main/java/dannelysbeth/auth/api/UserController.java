ea845bc46f4e81dbae6e807384786b750568d9be
Dannelysbeth <danielaf@onet.pl>
2024-07-10 22:48:52 +0200

    added user controller part 2

--- src/main/java/dannelysbeth/auth/api/UserController.java
+++ src/main/java/dannelysbeth/auth/api/UserController.java
@@ -1,4 +1,14 @@
 package dannelysbeth.auth.api;
 
+import dannelysbeth.auth.service.declaration.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+
 }
