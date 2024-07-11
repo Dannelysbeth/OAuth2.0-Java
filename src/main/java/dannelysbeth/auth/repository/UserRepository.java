3f497cb4342eb8760255560b4267fb73d2d2d8bd
Dannelysbeth <danielaf@onet.pl>
2024-07-11 23:52:11 +0200

    changed to MongoRepository

--- src/main/java/dannelysbeth/auth/repository/UserRepository.java
+++ src/main/java/dannelysbeth/auth/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package dannelysbeth.auth.repository;
 
 import dannelysbeth.auth.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends MongoRepository<User, Long> {
 }
