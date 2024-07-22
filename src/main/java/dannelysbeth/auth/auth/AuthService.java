5f4b1cd3e450b6042cc6a7472d110974217709f8
Dannelysbeth <danielaf@onet.pl>
2024-07-22 23:51:53 +0200

    changed auth filter as param

--- src/main/java/dannelysbeth/auth/auth/AuthService.java
+++ src/main/java/dannelysbeth/auth/auth/AuthService.java
@@ -1,8 +1,10 @@
 package dannelysbeth.auth.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthService {
-    AuthFilter filter;
+    private final AuthFilter filter;
 }
