402719d29773e12dc31d32060b95a76ef2ae38e3
Dannelysbeth <danielaf@onet.pl>
2024-07-29 23:28:58 +0200

    auth service

--- src/main/java/dannelysbeth/auth/auth/AuthController.java
+++ src/main/java/dannelysbeth/auth/auth/AuthController.java
@@ -10,4 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+
+    ///method to implement data
+
+
 }
