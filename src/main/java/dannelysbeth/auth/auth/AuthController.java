89d4e972d444b17067bfc529ec408a0b8cead3cc
Dannelysbeth <danielaf@onet.pl>
2024-08-01 23:51:05 +0200

    added login method

--- src/main/java/dannelysbeth/auth/auth/AuthController.java
+++ src/main/java/dannelysbeth/auth/auth/AuthController.java
@@ -1,6 +1,7 @@
 package dannelysbeth.auth.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,11 @@ public class AuthController {
 
 
     ///method to implement data
+    @GetMapping("/login")
+    public void login(){
+
+    }
+
 
 
 }
